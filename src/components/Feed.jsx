137bcb42a297f31419144f1aa09bddead5138d3f
Tahsin Haider <tahsin3194@gmail.com>
2024-11-22 22:23:21 +0500

    working on feed page

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,16 +1,28 @@
 import { useState, useEffect } from "react";
-import { Box, createTheme, Stack, ThemeProvider, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
+import Sidebar from "./Sidebar";
 
 const Feed = () => {
   return (
-    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+    <Stack direction={{ sx: "column", md: "row" }}>
       <Box
         sx={{
           height: { sx: "auto", md: "92vh" },
           borderRight: "1px solid #3d3d3d",
           px: { sx: 0, md: 2 },
         }}
-      ></Box>
+      >
+        <Sidebar/>
+        <Typography className="copyright" variant="body2" sx={{mt: 1.5, color: '#fff'}}>
+          Copyright 2024 YT Clone
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{overflowY: 'auto', height: '100vh',flex:2}}>
+        <Typography variant="h4" fontWeight='bold' mb={2} sx={{color: 'white'}}>
+          New <span style={{ color: '#FC1503' }}>videos</span>
+        </Typography>
+      </Box>
     </Stack>
   );
 };
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,34 @@
+import {  Stack } from "@mui/material";
+import { categories } from "../utils/constants";
+
+const selectedCategory = 'New'
+
+const Sidebar = () => {
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        overflowY: "auto",
+        height: { sx: "auto", md: "95%" },
+        flexDirection: { md: "column" },
+      }}
+    >
+      {categories.map((category) => (
+        <button
+          className="category-btn"
+          key={category.name}
+          style={{ color: "white", margin: '6px 0px',
+            background: category.name === selectedCategory && '#FC1503'
+          }}
+        >
+          <span style={{ paddingRight: 10, color: category.name === selectedCategory ? 'white' : 'red',
+            opacity: category.name === selectedCategory ? 1 : 0.8
+          }}>{category.icon}</span>
+          <span>{category.name}</span>
+        </button>
+      ))}
+    </Stack>
+  );
+};
+
+export default Sidebar;
